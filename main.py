8e41899dade533fdbb13c04b1fbb8ccef870f579
kareem171 <34690989+kareem171@users.noreply.github.com>
2018-05-17 14:36:40 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+from room import Room
+
+
+kitchen = Room("Kitchen")
+kitchen.set_description("A desk and dirty room buzzing with flies.")
+
+
+dining_hall = Room("Dining Hall")
+dining_hall.set_description("A large room with ornate golden decorations on each wall.")
+
+
+ballroom = Room("Ballroom")
+ballroom.set_description("A vast room with a shiny floor. Huge candlesticks guard the entrance.")
+
+
+
+dining_hall.link_room(kitchen, "north")
+dining_hall.link_room(ballroom, "west")
+ballroom.link_room(dining_hall, "east")
+
+current_room = kitchen
+while True:
+    print('/n')
+    current_room.get_details()
+    command = input('> ')
+    current_room = current_room.move(command)
